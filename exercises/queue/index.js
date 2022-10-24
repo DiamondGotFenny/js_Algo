aaa2f1644bef1ad1a09b96ce130e966512275763
Diamond Huang <diamond_huang@epam.com>
2022-10-24 14:41:05 +0800

    queue

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,90 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+/* class Queue {
+  constructor() {
+    this.data = [];
+  }
+  add(record) {
+    this.data.push(record);
+  }
+  remove() {
+    return this.data.shift();
+  }
+} */
 
+//without using class
+/* function Queue() {
+    var collection = [];
+    this.print = function () {
+      console.log(collection);
+    }
+    this.enqueue = function (element) {
+      collection.push(element);
+    }
+    this.dequeue = function () {
+      return collection.shift();
+    }
+    this.front = function () {
+      return collection[0];
+    }
+  
+    this.isEmpty = function () {
+      return collection.length === 0;
+    }
+    this.size = function () {
+      return collection.length;
+    }
+  } */
+
+//without using array
+// We create a class for each node within the queue
+class Node {
+  // Each node has two properties, its value and a pointer that indicates the node that follows
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+// We create a class for the queue
+class Queue {
+  // The queue has three properties, the first node, the last node and the stack size
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  // The enqueue method receives a value and adds it to the "end" of the queue
+  add(val) {
+    var newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+  // The dequeue method eliminates the element at the "beginning" of the queue and returns its value
+  remove() {
+    if (!this.first) return undefined;
+
+    var temp = this.first;
+    if (this.first === this.last) {
+      this.last = undefined;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+/* const newQ = new Queue();
+newQ.add('value 1');
+newQ.add('value 2');
+newQ.add('value 3');
+console.log(newQ.first);
+console.log(newQ.last);
+console.log(newQ.size); */
 module.exports = Queue;
