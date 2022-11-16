36fedfaf2599cd192a7c4435b397a1a46293893e
Diamond Huang <diamond_huang@epam.com>
2022-11-16 22:51:45 +0800

    bubble sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,19 +2,23 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temForLesserEle = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temForLesserEle;
+      }
+    }
+  }
+  return arr;
 }
+bubbleSort([100, -40, 500, -124, 0, 21, 7]);
 
-function selectionSort(arr) {
+function selectionSort(arr) {}
 
-}
+function mergeSort(arr) {}
 
-function mergeSort(arr) {
-
-}
-
-function merge(left, right) {
-
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
