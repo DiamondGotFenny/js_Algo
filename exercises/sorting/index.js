5d64da58913958dddb37f5e0d4aa63d4edfcbb53
Diamond Huang <diamond_huang@epam.com>
2022-11-19 23:20:23 +0800

    sorting

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -32,8 +32,28 @@ function selectionSort(arr) {
   return arr;
 }
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+  let center = Math.floor(arr.length / 2);
+  let leftArr = arr.slice(0, center);
+  let rightArr = arr.slice(center);
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
+}
 
-function merge(left, right) {}
+function merge(left, right) {
+  let result = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+  return [...result, ...left, ...right];
+}
+
+mergeSort([100, -40, 500, -124, 0, 21, 7]);
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
