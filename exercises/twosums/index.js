44d420b78271b3ff2cd2be0a3448f50b2d8a2e2c
diamondcn923 <diamondcn923@gmail.com>
2022-11-28 23:51:45 +0800

    two sums hash map solution

--- exercises/twosums/index.js
+++ exercises/twosums/index.js
@@ -6,6 +6,21 @@ if there is no solution, return null as well.
 You can return the answer in any order. */
 
 function twoSum(arr, target) {
+  const numsMap = {};
+  for (let p = 0; p < arr.length; p++) {
+    const currentMapVal = numsMap[arr[p]];
+    if (currentMapVal >= 0) {
+      return [currentMapVal, p];
+    } else {
+      const numberToFind = target - arr[p];
+      numsMap[numberToFind] = p;
+    }
+  }
+  return null;
+}
+module.exports = twoSum;
+
+/* function twoSum(arr, target) {
   for (let p1 = 0; p1 < arr.length; p1++) {
     let numberToFind = target - arr[p1];
     for (let p2 = p1 + 1; p2 < arr.length; p2++) {
@@ -15,5 +30,4 @@ function twoSum(arr, target) {
     }
   }
   return null;
-}
-module.exports = twoSum;
+} */
