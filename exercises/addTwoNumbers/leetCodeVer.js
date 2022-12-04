cc26021ac314be40772cae89b03669064f4afade
diamondcn923 <diamondcn923@gmail.com>
2022-12-04 22:51:47 +0800

    add two sum leetcode create

---
+++ exercises/addTwoNumbers/leetCodeVer.js
@@ -0,0 +1,7 @@
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+const addTwoNumbers = function (l1, l2) {};
+
+module.exports = { addTwoNumbers, ListNode };
